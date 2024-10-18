608b64667c0f0e773dbad2be0b3cfa7240481a2e
vigneshSrinivasan2005 <vignesh.2005.10.10@gmail.com>
2024-10-17 20:02:14 -0700

    added tailwind and header

---
+++ src/lib/Toolbar.svelte
@@ -0,0 +1,18 @@
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <span class="font-semibold text-xl tracking-tight">Bhojan Xpress</span>
+    </div>
+    <div class="flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="#responsive-header" class="text-lg text-teal-200 hover:text-white mr-2 md:mr-4 lg:mr-6">
+          Menu
+        </a>
+        <a href="#responsive-header" class="text-lg text-teal-200 hover:text-white mr-2 md:mr-4 lg:mr-6">
+          Pricing
+        </a>
+        <a href="#responsive-header" class="text-lg text-teal-200 hover:text-white mr-2 md:mr-4 lg:mr-6">
+          Get Started
+        </a>
+      </div>
+    </div>
+</nav>
